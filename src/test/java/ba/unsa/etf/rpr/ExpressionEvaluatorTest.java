28ccd9aeb0c2509e5323bea54c9a2990a8c5ebaf
Selma <shrustic1@etf.unsa.ba>
2022-11-19 19:26:02 +0100

    added testingInputFormat1

--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ExpressionEvaluatorTest {
+
+    private ExpressionEvaluator e = new ExpressionEvaluator();
     @Test
     public void testingExpression() throws RuntimeException {
-        ExpressionEvaluator e = new ExpressionEvaluator();
+
         assertEquals(101, e.evaluate("( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )"));
         assertEquals(11, e.evaluate("( 1 + ( ( 2 + 3 ) * ( sqrt 4 ) ) )"));
         assertEquals(21, e.evaluate("( 1 + ( ( 4 * 5 ) / ( 3 - 2 ) ) )"));
     }
     @Test
-    public void test() throws RuntimeException{
-        ExpressionEvaluator e = new ExpressionEvaluator();
+    public void testingInputFormat1() throws RuntimeException{
         assertThrows(Exception.class, () -> { e.evaluate("(1+2)");});
-        assertThrows(Exception.class, () -> { e.evaluate("sklj");});
     }
+
+
 }
