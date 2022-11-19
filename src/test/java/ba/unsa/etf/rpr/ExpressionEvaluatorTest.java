fb883c9a507243e6e525642445824bc4bbb03130
Selma <shrustic1@etf.unsa.ba>
2022-11-19 19:41:53 +0100

    added testingIsOpsEmpty

--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -8,34 +8,40 @@ public class ExpressionEvaluatorTest {
 
     private ExpressionEvaluator e = new ExpressionEvaluator();
     @Test
-    public void testingExpression() throws RuntimeException {
+    void testingExpression() throws RuntimeException {
 
         assertEquals(101, e.evaluate("( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )"));
         assertEquals(11, e.evaluate("( 1 + ( ( 2 + 3 ) * ( sqrt 4 ) ) )"));
         assertEquals(21, e.evaluate("( 1 + ( ( 4 * 5 ) / ( 3 - 2 ) ) )"));
     }
     @Test
-    public void testingInputFormat1() throws RuntimeException{
+    void testingInputFormat1() throws RuntimeException{
         assertThrows(Exception.class, () -> { e.evaluate("(1+2)");});
     }
 
     @Test
-    public void testingInputFormat2() throws RuntimeException{
+    void testingInputFormat2() throws RuntimeException{
         assertThrows(Exception.class, () -> { e.evaluate("sklj");});
     }
 
     @Test
-    public void testingInputFormat3() throws RuntimeException{
+    void testingInputFormat3() throws RuntimeException{
         assertThrows(Exception.class, () -> { e.evaluate("( ( 1 + 2 ) ) )");});
     }
 
     @Test
-    public void testingEmptyInput() throws RuntimeException{
+    void testingEmptyInput() throws RuntimeException{
         assertThrows(Exception.class, () -> { e.evaluate(" ");});
     }
 
     @Test
-    public void testingInputFormat4() throws RuntimeException{
+    void testingInputFormat4() throws RuntimeException{
         assertThrows(Exception.class, () -> { e.evaluate(" 1 + 2 ");});
     }
+
+    @Test
+    void testingIsOpsEmpty(){
+        assertTrue(e.isOpsEmpty());
+    }
+
 }
